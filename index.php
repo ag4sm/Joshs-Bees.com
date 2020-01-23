48a49da5315d4c3fda38b823a35ff8ce1cbe0b5d
ag4sm <56977525+ag4sm@users.noreply.github.com>
2020-01-23 16:33:44 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>This site is brand new</title>
+  <style>
+
+	/*bootstrap CSS link*/
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  html {
+    font-size: 62.5%;
+	min-height: 100%;
+  }
+  
+  header {
+	background-image: url("/images/pulled-comb.png");
+	padding-top: 1rem;
+	padding-bottom: 2rem;
+	width: 100%;
+  }
+  
+  footer {
+	width: 100%;
+	position: fixed;
+	left: 0;
+	bottom: 0;
+	text-align: center;
+	background-color: #ff8000;
+	color: white;
+  }
+  
+  .break {
+	clear:both;
+  }
+  
+  .left {
+	position: relative;
+	top: 8em;
+	width: 30%;
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 1.6rem;
+    color: #353535;
+	background: #C02425;  /* fallback for old browsers */
+	background: -webkit-linear-gradient(to right, #F0CB35, #C02425);  /* Chrome 10-25, Safari 5.1-6 */
+	background: linear-gradient(to right, #F0CB35, #C02425); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
+
+  h1,h3 {
+    font-family: 'Montserrat', sans-serif;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    text-transform: uppercase;
+	color: white;
+  }
+
+  h1 {
+    margin: 0rem 0 1rem;
+    font-size: 3rem;
+	color: white;
+  }
+
+  h3 {
+    font-weight: normal;
+    font-size: 1.4rem;
+	font-weight: bold
+  }
+  
+  img {
+	  padding: 5px;
+  }
+  
+  p {
+	margin: 1rem;
+	color: white;
+  }
+  
+  </style>
+</head>
+<body>
+
+	<header>
+		<h1>Welcome to Josh's Bees</h1>
+		<h3>Bee farm in southern Jones County Georgia</h3>
+		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	</header>
+
+	<img src="/images/me.JPG" width="150px" align="left" />
+
+	<p>I got interested in beekeeping in 2018 when helping my dad.  He got 2 hives and I helped him occasionally by holding tools, smoking them, lifting boxes, and such.  I just could not get enough of watching God's 
+		miraculous work at hand in those little creatures.  Everything from watching them build comb to laying eggs and storing nectar which they then turned into honey.  I was hooked and still they never cease to amaze me.  
+	</p>
+
+	<article class="left">
+	
+		<img src="/images/drawncomb1.jpg" width="250px" />
+		<img src="/images/drawncomb2.jpg" width="250px" />
+
+	</article>
+
+	<footer>
+		<h6>Design by Josh Scoville, January 2020</h6>
+	</footer>
+
+	<!--bootstrap scripts-->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<!--end of bootstrap scripts-->
+
+</body>
+</html>
