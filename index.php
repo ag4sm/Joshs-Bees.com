3f1d9788a821c8e6eeb422d8a2b0da82c2610520
ag4sm <56977525+ag4sm@users.noreply.github.com>
2020-05-15 16:27:09 -0400

    Use of bootstrap and jQuery
    I integrated bootstrap responsiveness a bit better.  Also, I integrated a jQuery UI accordion for the middle column of inspections.  I also integrated a bootstrap Carousel for the picture images in the left column.

--- index.php
+++ index.php
@@ -2,111 +2,109 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+  <link rel="stylesheet" type="text/css" href = "stylesheet.css">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>This site is brand new</title>
-  <style>
-
-	/*bootstrap CSS link*/
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-  html {
-    font-size: 62.5%;
-	min-height: 100%;
-  }
-  
-  header {
-	background-image: url("/images/pulled-comb.png");
-	padding-top: 1rem;
-	padding-bottom: 2rem;
-	width: 100%;
-  }
-  
-  footer {
-	width: 100%;
-	position: fixed;
-	left: 0;
-	bottom: 0;
-	text-align: center;
-	background-color: #ff8000;
-	color: white;
-  }
-  
-  .break {
-	clear:both;
-  }
-  
-  .left {
-	position: relative;
-	top: 8em;
-	width: 30%;
-  }
-  
-  body {
-    margin: 0;
-    padding: 0;
-    font-size: 1.6rem;
-    color: #353535;
-	background: #C02425;  /* fallback for old browsers */
-	background: -webkit-linear-gradient(to right, #F0CB35, #C02425);  /* Chrome 10-25, Safari 5.1-6 */
-	background: linear-gradient(to right, #F0CB35, #C02425); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  }
-
-  h1,h3 {
-    font-family: 'Montserrat', sans-serif;
-    text-align: center;
-    margin: 0;
-    padding: 0;
-    text-transform: uppercase;
-	color: white;
-  }
-
-  h1 {
-    margin: 0rem 0 1rem;
-    font-size: 3rem;
-	color: white;
-  }
-
-  h3 {
-    font-weight: normal;
-    font-size: 1.4rem;
-	font-weight: bold
-  }
-  
-  img {
-	  padding: 5px;
-  }
-  
-  p {
-	margin: 1rem;
-	color: white;
-  }
-  
-  </style>
+  <title>Josh's Bees</title>
 </head>
 <body>
 
-	<header>
-		<h1>Welcome to Josh's Bees</h1>
-		<h3>Bee farm in southern Jones County Georgia</h3>
-		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-	</header>
+<div class="conainer">
 
-	<img src="/images/me.JPG" width="150px" align="left" />
-
-	<p>I got interested in beekeeping in 2018 when helping my dad.  He got 2 hives and I helped him occasionally by holding tools, smoking them, lifting boxes, and such.  I just could not get enough of watching God's 
-		miraculous work at hand in those little creatures.  Everything from watching them build comb to laying eggs and storing nectar which they then turned into honey.  I was hooked and still they never cease to amaze me.  
-	</p>
-
-	<article class="left">
+	<div class="row">
+		<header class="col">
+			<h1>Welcome to Josh's Bees</h1>
+			<h3>Bee farm in southern Jones County Georgia</h3>
+		</header>
+	</div>
+	<div class="row">
+		<div class="col">
+			<img width="150" src="/images/me.JPG" align="left" />
 	
-		<img src="/images/drawncomb1.jpg" width="250px" />
-		<img src="/images/drawncomb2.jpg" width="250px" />
+			<p>I got interested in beekeeping in 2018 when helping my dad.  He got 2 hives and I helped him occasionally by holding tools, smoking them, lifting boxes, and such.  I just could not get enough of watching God's 
+				miraculous work at hand in those little creatures.  Everything from watching them build comb to laying eggs and storing nectar which they then turned into honey.  I was hooked and still they never cease to amaze me.  
+			</p>
+		</div>
+	</div>
+	<div class="clear"></div>
+	<div class="row">
+		
+		<!--Left Column-->
+		<div class="col-md-4">
 
-	</article>
+			<p>I set up the hive and bought a nuc in mid May 2019.  I spent the summer learning a lot and watching my bees.</p>
+			
+				<!--Code for Carousel from https://getbootstrap.com/docs/4.0/components/carousel/ -->
+				<div id="carousel1" class="carousel slide" data-ride="carousel">
+				  <div class="carousel-inner">
+					<div class="carousel-item active">
+					  <img class="d-block w-300" src="/images/drawncomb1.jpg" alt="First slide">
+					</div>
+					<div class="carousel-item">
+					  <img class="d-block w-300" src="/images/drawncomb2.jpg" alt="Second slide">
+					</div>
+				  </div>
+				  <a class="carousel-control-prev" href="#carousel1" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				  </a>
+				  <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				  </a>
+				</div>
+		
+		</div>
+        
+		<!--Center Column-->
+		<div class="col-md-4">
+		
+			<div id="logtitle">
+				<h3>2020 Inspection Report</h3>
+			</div>
+		
+			<div class="ui-accordion">
+				<h3 class="log">Feb 9 2020</h3>
+					<div>
+						<p>First quick inspection of this year.  I wanted to get in and see how they were doing over the winder.  Last check was early December 2019 and there was not much brood capped.  This time, however, since the days are getting longer, the queen has several frames in the middle of the top box with fully capped brood.  There is a healthy amount of bees in the hive as well.  Today was a sunny day, light wind and mild temperatures.  The bees were calm, but with healthy activity and still good on honey stores.  Dry sugar on top has not been eaten at all.</p>
+						<br />
+					</div>
+				<h3 class="log">March 1 2020</h3>
+					<div>
+						<p>Today was mostly cloudy and cool. High in the low 60's, so I knew the girls would be cranky and I suited up for the occasion, gloves and all.  Objective for today was to swap brood chambers, check food stores, and add beetle barns to each layer: bottom board, top of bottom box, and top of top box.  I did a quick frame inspection of the top box.  Saw good capped brood pattern on several frames.  Hive had healthy supply of bees which stands to reason after the amount of capped brood I saw in February.  I did not see any swarm cells, but I only did a quick glance over as I was swapping boxes.  They were getting angry and were all up in my face.  If I had not had on gloves and full suit, I would definitely have gotten popped multiple times.  They were also eating the dry sugar pretty good, but still have a little capped honey in the hive.</p>
+						<br />
+					</div>
+				<h3 class="log">March 2 2020</h3>
+					<div>
+						<p>I added a Boardman entrance feeder today with some 1:1 syrup to begin feeding.  Plan to add honey super end of March.</p>
+						<br />
+					</div>
+			</div>
+	
+		</div>
+        
+		<!--Right Column-->
+		<div class="col-md-4">
+        
+			<h3>Links of my Favorite Sites</h3>
+                                
+				<ul>
+					<li><a href="http://www.barnyardbees.com">Barnyard Bees</a></li>
+					<li><a href="http://www.dadant.com">Dadant</a></li>
+					<li><a href="http://www.fatbeeman.com">Dixie Bee Supply</a></li>
+				</ul>
+        
+		</div>
+	
+	</div>
+</div>
 
-	<footer>
-		<h6>Design by Josh Scoville, January 2020</h6>
+	<footer id="footer" class="footer mt-auto py-3">
+		<div class="container">
+			<span class="text">Design by Josh Scoville, January 2020</span>
+		</div>
 	</footer>
 
 	<!--bootstrap scripts-->
@@ -114,6 +112,23 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<!--end of bootstrap scripts-->
-
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+	<script type="text/javascript"> 
+		$(".ui-accordion").accordion({
+			collapsible: true,
+			heightStyle: 'panel'
+		});
+		
+		$(".log").hover(function(){
+			$(this).css("color","orange");
+		}, function(){
+			$(this).css("color","white");
+		});
+		
+		$("#carousel1").carousel({
+			interval: 2000
+		});
+	</script>
 </body>
 </html>
